a48557d1cc509e16311b81b90fa99ca7271fb56a
Will Devon-Sand <willds40@gmail.com>
2016-05-20 12:52:02 -0400

    first app madae

---
+++ ShameOnYou/ShameOnYou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShameOnYou
+//
+//  Created by Will Devon-sand on 5/20/16.
+//  Copyright © 2016 Will Devon-sand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShameOnYou/ShameOnYou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShameOnYou
+//
+//  Created by Will Devon-sand on 5/20/16.
+//  Copyright © 2016 Will Devon-sand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
